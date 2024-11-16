2fc7d9d934a201ac4decf7eff4f28363ee2edb0b
Deemoxzj <127976173+Deemoxzj@users.noreply.github.com>
2024-11-16 14:54:12 +0800

    Update Update-mesa.sh

--- Update-mesa.sh
+++ Update-mesa.sh
@@ -2,16 +2,26 @@ sudo dpkg --add-architecture arm64
 apt update
 apt install vim sudo -y
 apt install apt-transport-https ca-certificates
-sudo apt install gcc-aarch64-linux-gnu g++-aarch64-linux-gnu cmake:arm64 meson:arm64 git wget
-sudo apt install python3:arm64 python3-yaml:arm64 python3-mako:arm64
+sudo apt install gcc-aarch64-linux-gnu g++-aarch64-linux-gnu cmake meson git wget -y
+sudo apt install python3 python3-yaml python3-mako -y
+sudo apt install libzstd-dev:armhf libdrm-dev:armhf libudev-dev:armhf libelf-dev:armhf valgrind:armhf bison:armhf byacc:armhf libreadline-dev:armhf libedit-dev:armhf
+ flex:armhf
 
-export CC=aarch64-linux-gnu-gcc
-export CXX=aarch64-linux-gnu-g++
-export PKG_CONFIG_PATH=/usr/aarch64-linux-gnu/lib/pkgconfig
-export PKG_CONFIG_LIBDIR=/usr/aarch64-linux-gnu/lib/pkgconfig
-export CROSS_COMPILE=aarch64-linux-gnu-
-export ARCH=arm64
 
 cd ~
+echo '[binaries]
+c = 'arm-linux-gnueabihf-gcc'
+cpp = 'arm-linux-gnueabihf-g++'
+ar = 'arm-linux-gnueabihf-ar'
+strip = 'arm-linux-gnueabihf-strip'
+pkgconfig = 'arm-linux-gnueabihf-pkg-config'
+[host_machine]
+system = 'linux'
+cpu_family = 'arm'
+cpu = 'arm'
+endian = 'little''>>cross_file_armhf.txt
+
+
 git clone https://gitlab.freedesktop.org/mesa/mesa.git
 cd mesa
+meson b32 --cross-file=~/cross_file_armhf.txt -Dgallium-drivers=freedreno,zink,virgl,d3d12,softpipe  -Dvulkan-drivers=freedreno -Dgallium-nine=false -Dfreedreno-kmds=kgsl,msm -Dglx=dri -Dxlib-lease=enabled -Dplatforms=x11,wayland -Dvulkan-beta=true -Dvideo-codecs=all -Dglx-direct=true -Dtools=drm-shim,freedreno -Dopengl=true -Dosmesa=true -Dgles1=enabled -Dgles2=enabled -Degl-native-platform=x11 -Degl=enabled -Dpower8=enabled -Dgallium-d3d12-video=enabled -Dgallium-d3d12-graphics=enabled  -Dxmlconfig=enabled -Dgbm=enabled --prefix=/opt/mesa
